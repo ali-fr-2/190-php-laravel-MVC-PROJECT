5e4fd4f4300bc3e10f1252fb798a8561685ea495
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-24 23:36:49 +0330

    197-controller

--- app/controllers/Courses.php
+++ app/controllers/Courses.php
@@ -1,12 +1,12 @@
 <?php 
 
-class Courses{
+class Courses extends Controllers{
     public function __construct()
     {
         echo "courses";
     }
     public function index(){
-
+        $this->view("Courses");
     }
 
     public function edit($id){
